6f2981a08fbe633aa2737e4ec7e053c5b09a9cb3
Kirill Ki <kirillkalapov@gmail.com>
2022-02-15 23:45:52 +0100

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by  Mr.Ki on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
