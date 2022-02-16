588d189b65302beead6df65cff80d428c66249ce
Kirill Ki <kirillkalapov@gmail.com>
2022-02-16 01:02:41 +0100

    first

---
+++ MVP/MainPresenter.swift
@@ -0,0 +1,37 @@
+//
+//  MainPresenter.swift
+//  MVP
+//
+//  Created by  Mr.Ki on 16.02.2022.
+//
+
+import Foundation
+
+protocol MainViewProtocol: class {
+    func setGreeting(greeting: String)
+}
+
+protocol MainViewPresenterProtocol: class {
+    init(view: MainViewProtocol, person: Person)
+        func showGreeting()
+    
+}
+
+class MainPresenter: MainViewPresenterProtocol {
+
+    let view: MainViewProtocol
+    let person: Person
+    
+    required init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+
+    
+    func showGreeting() {
+        let greeting = self.person.firstName + " " + self.person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+    
+    
+}
---
+++ MVP/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  MVP
+//
+//  Created by  Mr.Ki on 15.02.2022.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    
+}
